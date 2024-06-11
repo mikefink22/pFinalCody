da622a7a49ce53ccae8bf6fa5b0a4711ce44d98a
Miguel Flores <m.ang.flores86@gmail.com>
2024-06-10 22:53:46 -0300

    Muchos cambios

---
+++ pages/main/script.js
@@ -0,0 +1,10 @@
+function showPopup(message) {
+    document.getElementById('popup-message').innerText = message;
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
